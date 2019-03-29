32c29750eaa3cf13f96a1071d6d9f8d5eafd055e
Ben Rogers <33623970+ezilybored@users.noreply.github.com>
2019-03-29 10:57:50 +0000

    heroku not connecting to databse attempting a fix

--- application.py
+++ application.py
@@ -12,12 +12,15 @@ import json
 app = Flask(__name__)
 
 # Socket setup
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = "a super special secreat"
 socketio = SocketIO(app, manage_session=False) # manage_session=False hands the session handling to flask
 
 # Database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yvmypvesmwtmzo:e9224998c3a09576fd8640942a82428f33415c6e36a2e49c6b91e9d91b9e037d@ec2-54-75-230-41.eu-west-1.compute.amazonaws.com:5432/db53552ukasjon'
 db = SQLAlchemy(app)
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
